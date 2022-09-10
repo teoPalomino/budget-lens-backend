65dd701ac78539245e8c39831beaafab1ca597b2
Massimo <mlopez9876@gmail.com>
2022-09-10 19:47:12 -0400

    Task_BUD-48_Django_Backend_Setup * Created Virtual env using pipenv. * Created simple Django project, nothing pretty about it. * Setup Local postgres db called bl_test. May run into issues it Django doesn't create new db for everyone else. * Setup Django REST API and made routes for testing the setup of the REST API ('registerEndpoint/' and 'loginEndpoint/').

---
+++ users/views.py
@@ -0,0 +1,40 @@
+from rest_framework import viewsets, generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import password_validators_help_texts
+from .serializers import *
+
+
+class RegisterAPI(generics.GenericAPIView):
+    """API for registering a new user"""
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            # saves user and its data
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            # creates token for that particular user
+            "token": AuthToken.objects.create(user)[1],
+            "passwordValidators": password_validators_help_texts(password_validators=None)
+        })
+
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = ()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            # saves user and its data
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            # creates token for that particular user
+            "token": AuthToken.objects.create(user)[1]
+        })
