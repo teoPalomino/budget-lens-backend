2c518a13da5a5c69ec9066d98b8596e9cf313ed6
Amir Sadeqi <amirsadeqi@Amirs-MacBook-Air.local>
2022-09-20 21:28:27 -0400

    use dotenv as enviroment variable parser and edit settings.py to use EV

--- budget_lens_backend/settings.py
+++ budget_lens_backend/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,15 +103,15 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql',
 
-        'NAME': 'bud_local_db',
+        'NAME': os.getenv('POSTGRES_DB'),
 
-        'USER': 'postgres',
+        'USER': os.getenv('POSTGRES_PASSWORD'),
 
-        'PASSWORD': '9876',
+        'PASSWORD': os.getenv('POSTGRES_USER'),
 
-        'HOST': 'localhost',
+        'HOST': os.getenv('POSTGRES_HOST'),
 
-        'PORT': '5432',
+        'PORT': os.getenv('POSTGRES_PORT'),
 
     }
 
