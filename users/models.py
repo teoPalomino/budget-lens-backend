0f181b04476d4053e4545f29171bf7656a2ab065
Massimo Lopez <mlopez9876@gmail.com>
2022-10-03 16:11:07 -0400

    feat: added telephone number, made password more secure.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# class CustomUser(User):
+#     User.first_name = models.CharField(max_length=150, blank=False)
+#     User.last_name = models.CharField(max_length=150, blank=False)
+#     User.email = models.EmailField(blank=False)
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['USERNAME']
+
+
+class UserProfile(models.Model):
+    """A User Profile with a phone number and a user model
+    """
+    user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
+    telephone_number = models.PositiveBigIntegerField()
--- users/views.py
+++ users/views.py
@@ -3,23 +3,26 @@ from rest_framework.response import Response
 from knox.models import AuthToken
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import password_validators_help_texts
+
+from users.models import UserProfile
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
 
 class RegisterAPI(generics.GenericAPIView):
     """API for registering a new user"""
-    queryset = User.objects.all()
+    queryset = UserProfile.objects.all()
     serializer_class = RegisterSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
+        user_profile = serializer.save()
         return Response({
             # saves user and its data
-            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "user": UserSerializer(user_profile.user, context=self.get_serializer_context()).data,
+            "telephone number": user_profile.telephone_number,
             # creates token for that particular user
-            "token": AuthToken.objects.create(user)[1],
+            "token": AuthToken.objects.create(user_profile.user)[1],
             "passwordValidators": password_validators_help_texts(password_validators=None)
         })
 
