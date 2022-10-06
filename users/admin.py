3e36363d16e4914052a0b75d980acb6cffee9b18
Massimo Lopez <mlopez9876@gmail.com>
2022-10-05 23:32:46 -0400

    feat(djangoadmin): Putting model to django admin site.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in UserProfile._meta.get_fields()]
--- users/models.py
+++ users/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# class CustomUser(User):
-#     User.first_name = models.CharField(max_length=150, blank=False)
-#     User.last_name = models.CharField(max_length=150, blank=False)
-#     User.email = models.EmailField(blank=False)
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['USERNAME']
-
-
 class UserProfile(models.Model):
     """A User Profile with a phone number and a user model
     """
