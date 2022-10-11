c54fc6e0e7c8f9cd244b5806cba7397deb61a2dd
Nizar El Jurdi <nizareljurdi@gmail.com>
2022-10-11 13:51:09 -0400

    (feat): alter Friends table, progress on inviting friends

--- users/models.py
+++ users/models.py
@@ -14,5 +14,6 @@ class Friends(models.Model):
     A friends profile
     """
     main_user = models.ForeignKey(User, related_name='main_user', on_delete=models.CASCADE)
-    friend_user = models.ForeignKey(User, related_name='friend_user', on_delete=models.CASCADE)
+    friend_user = models.ForeignKey(User, related_name='friend_user', on_delete=models.CASCADE, null=True)
     confirmed = models.BooleanField(default=False)
+    temp_email = models.EmailField(max_length=254, null=True)
--- users/serializers.py
+++ users/serializers.py
@@ -80,14 +80,15 @@ class AddFriendsSerializer(serializers.ModelSerializer):
     """Validates the email for adding a friend"""
     class Meta:
         model = Friends
-        fields = ('friend_user', 'main_user', 'confirmed')
+        fields = '__all__'
 
     def create(self, validated_data):
         main_user = validated_data.pop('main_user')
         friend_user = validated_data.pop('friend_user')
         confirmed = validated_data.pop('confirmed')
+        temp_email = validated_data.pop('temp_email')
 
-        friend = Friends.objects.create(main_user=main_user, friend_user=friend_user, confirmed=confirmed)
+        friend = Friends.objects.create(main_user=main_user, friend_user=friend_user, confirmed=confirmed, temp_email=temp_email)
 
         return friend
 
