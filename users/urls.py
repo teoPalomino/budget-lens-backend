cb841f51e0343c161f87dab73b867673759f1f71
Isaac Muriuki <i_muriuk@live.concordia.ca>
2022-10-11 23:23:59 -0400

    feat: add friend request response API, added data to url args

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('userEndpoint/', views.UserAPI.as_view(), name='user_data'),
     path('logoutEndpoint/', views.LogoutAPI.as_view(), name='logout_user'),
     path('friend/', views.GetFriendsAPI.as_view(), name='get_friends'),
+    path('friend/<int:user_id>', views.GetFriendsAPI.as_view(), name='get_friends'),
     path('friend/add', views.AddFriendsAPI.as_view(), name='add_friends'),
+    path('friend/requestResponse/<int:friend_id>/<int:requestResponse>', views.FriendRequestResponseAPI.as_view(), name='friend_request_response'),
     path('friend/invite', views.InviteFriendsAPI.as_view(), name='invite_friends'),
-    path('friend/remove', views.RemoveFriendsAPI.as_view(), name='remove_friends')
+    path('friend/remove/<int:friend_id>', views.RemoveFriendsAPI.as_view(), name='remove_friends')
 ]
