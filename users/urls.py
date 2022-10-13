f2322da0449a42ff96c2a456ff9c01d15f4fb35e
Justin <62494960+ThatJustin@users.noreply.github.com>
2022-10-12 20:33:45 -0400

    Feat: bud 87/88 backend controller edit userprofile (#9)
    * feat: added user profile updating
    
    feat: added userupdate end point
    
    feat: implemented user profile update via PUT request
    
    feat: added user profile editing tests
    
    * fix: added status code to response
    
    fix: rename url path from userProfileUpdate to userprofile
    fix: changed UserUpdateAPI name to UserProfileAPI
    
    feat(UserProfile): changed telephone_number field to PhoneNumberField
    
    feat(UserProfileAPI): added telephone_number validation
    
    Requires new package and migration.
    Might also require a fresh database for migration to actually work.
    
    * fix: requirement missing
    
    * fix: requirement missing
    
    * fix: flake8 telling me off

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('registerEndpoint/', views.RegisterAPI.as_view(), name='register_user'),
     path('loginEndpoint/', views.LoginAPI.as_view(), name='login_user'),
     path('userEndpoint/', views.UserAPI.as_view(), name='user_data'),
+    path('userprofile/', views.UserProfileAPI.as_view(), name='user_profile'),
     path('logoutEndpoint/', views.LogoutAPI.as_view(), name='logout_user')
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from phonenumber_field.phonenumber import PhoneNumber
 from rest_framework import generics
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -30,7 +34,7 @@ class RegisterAPI(generics.GenericAPIView):
         return Response({
             # saves user and its data
             "user": user.data,
-            "telephone number": user_profile.telephone_number,
+            "telephone_number": str(user_profile.telephone_number),
             # creates token for that particular user
             # "token": AuthToken.objects.create(user_profile.user)[1],
             "token": token.key,
@@ -80,3 +84,63 @@ class LogoutAPI(APIView):
         return Response({
             "data": "Succesfully deleted"
         })
+
+
+class UserProfileAPI(generics.UpdateAPIView):
+    """Handles updating user profile information through a PUT request """
+    permission_classes = [IsAuthenticated, ]
+
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        username = request.data.get('username', "NONE")
+        first_name = request.data.get('first_name', "NONE")
+        last_name = request.data.get('last_name', "NONE")
+        email = request.data.get('email', "NONE")
+        telephone_number = request.data.get('telephone_number', "NONE")
+
+        # Makes sure all request input is valid
+        result, status_code = self.is_valid_request(username, first_name, last_name, email, telephone_number)
+
+        if result is None:  # no error, update can happen
+            user_profile = UserProfile.objects.get(user=request.user)
+            user_profile.user.username = username
+            user_profile.user.first_name = first_name
+            user_profile.user.last_name = last_name
+            user_profile.user.email = email
+            user_profile.user.save()
+
+            user_profile.telephone_number = telephone_number
+            user_profile.save()
+
+            result, status_code = {"response": "Success"}, HTTP_200_OK
+        return Response(result, status=status_code)
+
+    @staticmethod
+    def is_valid_request(username, first_name, last_name, email, telephone_number):
+        """Checks if the request is valid by looking at the form data key/values and email format"""
+
+        # Ensure no missing key or values for the form data
+        expected_input = {'Username': username, 'First Name': first_name, 'Last Name': last_name, 'Email': email,
+                          'Phone Number': telephone_number}
+        missing_inputs = []
+        for key, value in expected_input.items():
+            if value == "NONE" or value == "":
+                missing_inputs.append(key)
+
+        if missing_inputs:
+            return {"response": "Missing field or value for " + str(missing_inputs) + "."}, HTTP_400_BAD_REQUEST
+
+        # Validate phone number
+        valid_telephone_number = PhoneNumber.from_string(telephone_number)
+        if not valid_telephone_number.is_valid():
+            return {"response": "Invalid phone number."}, HTTP_400_BAD_REQUEST
+
+        # Validate the email is a correct format
+        try:
+            validate_email(email)
+        except ValidationError:
+            return {"response": "Invalid email format."}, HTTP_400_BAD_REQUEST
+        else:
+            return None, HTTP_200_OK
