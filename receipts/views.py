5520a365362f88670d71c8f4ec3366ba2f14befd
Mohamed <67757119+mamineahmadi@users.noreply.github.com>
2022-10-13 00:57:11 -0400

    feat:store/upload scanned receipt images with a relative directory/path to their location

--- receipts/views.py
+++ receipts/views.py
@@ -7,7 +7,7 @@ from .serializers import ReceiptsSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 
 # make a user folder with the user_id as the name
-# make a sub folder inside that specific user's folder with the receipt_id as the name
+# make a sub-folder inside that specific user's folder with the receipt_id as the name
 # save the image inside the folder of the user's receipt with the current unix timestamp scan_date as the image file name
 
 
