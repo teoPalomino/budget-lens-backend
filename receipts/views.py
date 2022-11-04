cecd107e9a7de413d357474b66d86ebd2f7dceb7
shirlywy <2406613854@qq.com>
2022-11-04 18:12:17 -0400

    remove time of sale field from filter

--- receipts/views.py
+++ receipts/views.py
@@ -71,7 +71,7 @@ class DefaultReceiptPaginationAPIListView(generics.ListAPIView):
     filterset_class = ReceiptsFilter
     ordering_fields = '__all__'
     search_fields = ['scan_date', 'coupon', 'merchant__name', 'location', 'total', 'tax', 'tip', 'currency',
-                     'important_dates', 'time_of_sale']
+                     'important_dates']
 
     def get(self, request, *args, **kwargs):
         """
