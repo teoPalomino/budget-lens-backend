795a0c33cecd1870f505bf59756f9ad5044a0e80
Isaac Muriuki <i_muriuk@live.concordia.ca>
2022-11-14 19:35:48 -0500

    feat: delete item API

--- item/urls.py
+++ item/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('items/', views.GetItemsAPI.as_view(), name='create_item'), # Get items list
-    path('items/add/', views.AddItemAPI.as_view(), name='create_item'), # Add an item
-    path('items/<int:item_id>/', views.ItemDetailAPIView.as_view(), name='item_details'), # Observe details of an item
+    path('items/', views.GetItemsAPI.as_view(), name='create_item'),  # Get items list
+    path('items/add/', views.AddItemAPI.as_view(), name='create_item'),  # Add an item
+    path('items/<int:item_id>/', views.ItemDetailAPIView.as_view(), name='item_details'),  # Observe details of an item
+    path('items/delete/<int:item_id>', views.DeleteItemAPI.as_view(), name='delete_item'),  # Delete an item
 ]
--- item/views.py
+++ item/views.py
@@ -4,21 +4,24 @@ from rest_framework.decorators import api_view
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 from item.serializers import ItemSerializer, PutPatchItemSerializer
 from receipts.models import Receipts
 
 from .models import Item
 
+
 class AddItemAPI(generics.CreateAPIView):
     """ Adds item to a receipt for a user """
     serializer_class = ItemSerializer
     permission_classes = [IsAuthenticated]
+
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         item = serializer.save()
-        
+
         if Receipts.objects.filter(id=request.data["receipt_id"]).exists():
             return Response({
                 "receipt_id": item.receipt_id,
@@ -31,6 +34,7 @@ class AddItemAPI(generics.CreateAPIView):
             "Error": "Receipt does not exist"
         })
 
+
 class GetItemsAPI(generics.ListCreateAPIView):
     """ Gets list of items for a user """
     queryset = Item.objects.all()
@@ -42,7 +46,7 @@ class GetItemsAPI(generics.ListCreateAPIView):
         serializer = ItemSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    
+
 class ItemDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     """ details for an item """
     permission_classes = [IsAuthenticated]
@@ -52,3 +56,24 @@ class ItemDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     # Check: User can only delete their own items
     def get_queryset(self):
         return Item.objects.filter(user=self.request.user)
+
+
+class DeleteItemAPI(generics.DestroyAPIView):
+    """ Deletes an item """
+    permission_classes = [IsAuthenticated]
+    serializer_class = PutPatchItemSerializer
+    lookup_url_kwarg = 'item_id'
+
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+
+    def delete(self, request, *args, **kwargs):
+        if kwargs.get('item_id'):
+            item = self.get_queryset().filter(id=kwargs.get('item_id')).first()
+            if item.exists():
+                item.delete()
+                return Response({"response": "Item deleted successfully"}, status=HTTP_200_OK)
+            else:
+                return Response({"response": "Item not found"}, status=HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"response": "Item ID not specified"}, status=HTTP_400_BAD_REQUEST)
