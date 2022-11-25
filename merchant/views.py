e960d38cfc8f01c4922a918cf87f584cf81fb672
Isaac Muriuki <i_muriuk@live.concordia.ca>
2022-11-24 20:33:37 -0500

    fix flake8

--- merchant/views.py
+++ merchant/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK
 
 from .models import Merchant
 from .serializers import MerchantSerializer
