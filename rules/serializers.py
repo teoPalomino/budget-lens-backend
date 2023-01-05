b944414e8e0d101aa8dec8e7c27a824c25abefb4
Isaac Muriuki <isaacmm59@gmail.com>
2023-01-04 23:46:49 -0500

    Rules API completed (#54)
    * created rules table
    
    * feat: created rules table
    
    * finished rules app
    
    * fix flake8
    
    Co-authored-by: Isaac Muriuki <i_muriuk@live.concordia.ca>

---
+++ rules/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from rules.models import Rule
+
+
+class RuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rule
+        fields = '__all__'
+
+    def create(self, validated_data):
+        rule = Rule.objects.create(
+            user=validated_data['user'],
+            regex=validated_data['regex'],
+            category=validated_data['category'],
+            created_at=validated_data['created_at']
+        )
+        return rule
+
+
+class PutPatchRuleSerializer(serializers.ModelSerializer):
+    """Serializer for PutRules, used to update a users rules"""
+
+    class Meta:
+        model = Rule
+        fields = ('regex', 'category')
