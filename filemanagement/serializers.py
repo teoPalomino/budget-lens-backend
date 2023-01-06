d09e37be9faea9ad3018d2cf6c6a343a74585504
woshidiyi1228 <zty981228@gmail.com>
2023-01-05 22:22:05 -0500

    flake8 errors

--- filemanagement/serializers.py
+++ filemanagement/serializers.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
 from .models import Files
 
+
 class FilesSerializer(serializers.ModelSerializer):
 
-
- """File Upload"""
- class Meta:
-    model = Files
+    """File Upload"""
+    class Meta: model = Files
     fields = '__all__'
