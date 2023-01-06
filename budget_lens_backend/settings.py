008e818c252a8315ae8bf865dc0c115edb3392b4
shahdyousefak <shahdyousefak@gmail.com>
2023-01-06 08:15:08 -0500

    filler text

--- budget_lens_backend/settings.py
+++ budget_lens_backend/settings.py
@@ -79,8 +79,8 @@ REST_FRAMEWORK = {
 }
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Your Project API',
-    'DESCRIPTION': 'Your project description',
+    'TITLE': 'BudgetLens API',
+    'DESCRIPTION': 'BudgetLens:',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
     # OTHER SETTINGS
