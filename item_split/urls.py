7c248e1660fbba6a8821257d9ca2a4e9a4c77a55
Massimo Lopez <mlopez9876@gmail.com>
2023-01-16 23:53:59 -0500

    feat: `GetSharedUsersList` and `GetSharedAmount` routes

--- item_split/urls.py
+++ item_split/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('api/itemsplit/', views.AddItemSplitAPI.as_view(), name='add_item_split'),
+    path('api/itemsplit/sharedUsers/itemsplit_id=<itemsplit_id>/', views.GetSharedUsersList.as_view(), name='get_user_list'),
+    path('api/itemsplit/sharedAmount/itemsplit_id=<itemsplit_id>/', views.GetSharedAmount.as_view(), name='get_shared_amount'),
 ]
