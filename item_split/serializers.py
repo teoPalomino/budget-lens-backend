1cbc453711675d0903aa870bc5e32140e890b59a
Massimo Lopez <mlopez9876@gmail.com>
2023-01-25 10:41:41 -0500

    fix: Mo requested changes.

--- item_split/serializers.py
+++ item_split/serializers.py
@@ -9,18 +9,20 @@ class ItemSplitSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = ItemSplit
-        fields = '__all__'
+        fields = ('item', 'shared_user_ids', 'id')
 
     def create(self, validated_data):
         num_to_divide_by = len(validated_data['shared_user_ids'].split(','))
-        if validated_data['is_shared_with_item_user'] is True:
-            num_to_divide_by += 1
+
+        is_shared_with_item_user = False
+        if validated_data['item'].user.pk in list(map(int, validated_data['shared_user_ids'].split(','))):
+            is_shared_with_item_user = True
 
         shared_amount = validated_data['item'].price / num_to_divide_by
 
         return ItemSplit.objects.create(
             shared_user_ids=validated_data['shared_user_ids'],
-            is_shared_with_item_user=validated_data['is_shared_with_item_user'],
+            is_shared_with_item_user=is_shared_with_item_user,
             shared_amount=shared_amount,
             item=validated_data['item']
         )
