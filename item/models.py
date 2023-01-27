ac2c21d633b2b06843dfeff55b2edce63af374a2
Shahd Yousef <shahdyousefak@Shahds-MacBook-Pro.local>
2023-01-27 00:35:38 -0500

    fixing test

--- item/models.py
+++ item/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-from receipts.models import Receipts
+#from receipts.models import Receipts
+import receipts.models
 from category.models import Category
 from django.contrib.auth.models import User
 
@@ -10,7 +11,7 @@ class Item(models.Model):
     An Item from a receipt that contains a name, it's price, category and important dates
     """
     user = models.ForeignKey(User, related_name='item_user', on_delete=models.CASCADE)
-    receipt = models.ForeignKey(Receipts, related_name='receipts', on_delete=models.CASCADE)
+    receipt = models.ForeignKey(receipts.models.Receipts, related_name='receipts', on_delete=models.CASCADE)
     category_id = models.ForeignKey(Category, related_name='category', null=True, on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=36)
     price = models.DecimalField(max_digits=18, decimal_places=2)
