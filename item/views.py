c36b7775e24a83cc742d3082438eab095acca85f
Mateo <mateo.palomino21@hotmail.com>
2023-02-04 12:26:05 -0500

    Item list shows actual total list cost + fix error

--- item/views.py
+++ item/views.py
@@ -170,11 +170,10 @@ class PaginateFilterItemsView(generics.ListAPIView):
         item_list_response = super().get(request, *args, **kwargs)
         item_total_cost = 0
         items = Item.objects.filter(user=self.request.user)
-        total_value = 0
 
         if items.exists():
             for item in items:
-                total_value += item.price
+                item_total_cost += item.price
 
         # Try to turn page number to an int value, otherwise make sure the response returns an empty list
         try:
@@ -202,7 +201,7 @@ class PaginateFilterItemsView(generics.ListAPIView):
             return Response({
                 'page_list': [],
                 'total': 0,
-                'total Cost': total_value,
+                'total Cost': item_total_cost,
                 'description': "Invalid Page Number"
             }, status=HTTP_200_OK)
 
@@ -221,11 +220,6 @@ class PaginateFilterItemsView(generics.ListAPIView):
         for i, item in zip(queryset, page.object_list):
             item['scan_date'] = i.receipt.scan_date
             item['merchant_name'] = i.receipt.merchant.name
-
-        for i in page.object_list:
-            current_item_price = list(i.items())
-            item_total_cost += float(current_item_price[2][1])
-            if_pass_last_page = item_total_cost
 
         return Response({
             'page_list': page.object_list,
