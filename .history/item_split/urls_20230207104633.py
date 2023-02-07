784eed7b45fc09f553ef3a907e594d02140b1c8d
woshidiyi1228 <zty981228@gmail.com>
2023-02-07 10:51:36 -0500

    remove useless files

--- .history/item_split/urls_20230207104633.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/itemsplit/', views.AddItemSplitAPI.as_view(), name='add_item_split'),
-    path('api/itemsplit/sharedUsers/item_id=<item_id>/', views.GetSharedUsersList.as_view(), name='get_user_list'),
-    path('api/itemsplit/sharedAmount/item_id=<item_id>/', views.GetSharedAmount.as_view(), name='get_shared_amount'),
-    path('api/itemsplit/sharedAmountList/', views.get_share_amount_list, name='get_shared_amount_list'),
-]
