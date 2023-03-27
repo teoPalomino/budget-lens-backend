e050daeee77d7776c1f7771e3bc00ddc1281bd7f
amirsadeqi <amir@sadeghi.me>
2023-03-27 19:41:27 -0400

    feat: ignore tests in post save

--- receipts/models.py
+++ receipts/models.py
@@ -62,9 +62,10 @@ class Receipts(models.Model):
 
     @receiver(post_save, sender='receipts.Receipts')
     def post_save_receipt(sender, instance, created, *args, **kwargs):
-        from utility.analyze_receipt import analyze_receipts
-        from utility.categorize_line_items import categorize_line_items
-        if created:
-            instance.receipt_text = analyze_receipts(instance.receipt_image.path, instance)
-            categorize_line_items(instance)
-            instance.save()
+        if os.getenv('APP_ENV') != 'test':
+            from utility.analyze_receipt import analyze_receipts
+            from utility.categorize_line_items import categorize_line_items
+            if created:
+                instance.receipt_text = analyze_receipts(instance.receipt_image.path, instance)
+                categorize_line_items(instance)
+                instance.save()
